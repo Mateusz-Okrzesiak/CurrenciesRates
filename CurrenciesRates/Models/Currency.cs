11937665a97cdd1a87deb4a2d34b0881b0ac9a60
mateusz.okrzesiak <mateusz.okrzesiak@benson.pl>
2020-07-20 15:43:43 +0200

    Initial commit

---
+++ CurrenciesRates/Models/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrenciesRates.Models
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public string CurrencySymbol { get; set; }
+        public double AVGRate { get; set; }
+    }
+}
